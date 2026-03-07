2f8bd306e5f227fe4081fb0b0a5e4f88aeda578f
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2026-03-07 23:48:29 +0800

    refactor: drop md-consistent-table-width

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { version } from '../package.json'
 import i18nFlatKey from './i18n-flat-key'
 import jsoncInlineSpacing from './jsonc-inline-spacing'
 import jsxAttributeSpacing from './jsx-attribute-spacing'
-import mdConsistentTableWidth from './md-consistent-table-width'
 import mdOneSentencePerLine from './md-one-sentence-per-line'
 import noDependencyVersionPrefix from './no-dependency-version-prefix'
 import preferEarlyReturn from './prefer-early-return'
@@ -18,7 +17,6 @@ export default {
     'i18n-flat-key': i18nFlatKey,
     'jsonc-inline-spacing': jsoncInlineSpacing,
     'jsx-attribute-spacing': jsxAttributeSpacing,
-    'md-consistent-table-width': mdConsistentTableWidth,
     'md-one-sentence-per-line': mdOneSentencePerLine,
     'no-dependency-version-prefix': noDependencyVersionPrefix,
     'prefer-early-return': preferEarlyReturn,
